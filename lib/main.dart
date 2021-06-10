afb9ed81dd2a09e81ccffce01d277bbaf2c5f321
Ling Vu <vumanhling@icloud.com>
2021-06-10 22:43:14 +0200

    Add final screen after finish quiz

--- lib/main.dart
+++ lib/main.dart
@@ -13,45 +13,49 @@ class MyApp extends StatefulWidget {
 
 class _MyAppState extends State<MyApp> {
   int _questionIndex = 0;
+  final List questions = const [
+    {
+      'question': 'What\'s your favorite color?',
+      'answers': ['Black', 'Red', 'Green', 'Blue'],
+    },
+    {
+      'question': 'What\'s your favorite animal?',
+      'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
+    },
+    {
+      'question': 'What\'s your favorite food?',
+      'answers': ['Kebab', 'Pizza', 'Hamburger', 'Hot Dog'],
+    },
+  ];
 
   void _answerQuestion() {
     setState(() {
-      _questionIndex = 1;
+      _questionIndex++;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    List questions = [
-      {
-        'question': 'What\'s your favorite color?',
-        'answers': ['Black', 'Red', 'Green', 'Blue'],
-      },
-      {
-        'question': 'What\'s your favorite animal?',
-        'answers': ['Rabbit', 'Snake', 'Elephant', 'Lion'],
-      },
-      {
-        'question': 'What\'s your favorite food?',
-        'answers': ['Kebab', 'Pizza', 'Hamburger', 'Hot Dog'],
-      },
-    ];
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('My First App'),
         ),
-        body: Column(
-          children: <Widget>[
-            Question(questions[_questionIndex]['question']),
-            ...questions[_questionIndex]['answers'].map((answer) {
-              return Answer(
-                answerText: answer,
-                selectHandler: _answerQuestion,
-              );
-            }),
-          ],
-        ),
+        body: _questionIndex < questions.length
+            ? Column(
+                children: <Widget>[
+                  Question(questions[_questionIndex]['question']),
+                  ...questions[_questionIndex]['answers'].map((answer) {
+                    return Answer(
+                      answerText: answer,
+                      selectHandler: _answerQuestion,
+                    );
+                  }),
+                ],
+              )
+            : Center(
+                child: Text('You did it!'),
+              ),
       ),
     );
   }
